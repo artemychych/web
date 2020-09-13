0245c050e7beabc05097bca4fccee8bfcff41b23
artemychych <artemychmain@gmail.com>
2020-09-13 22:14:31 +0900

    buttons script.js added

---
+++ lab_1/js/script.js
@@ -0,0 +1,16 @@
+var date = new Date();
+var time = date.getHours();
+
+if (time<13) {
+    alert('Доброе утро');
+}
+
+else {
+    alert('Добрый день');
+}
+
+var a = 3 + 5;
+var l = new Array();
+var myArray = [];
+var people = ["Tom", "Grace", "Voldemar"]
+console.log(people);
