8c3ae8decd6f6fa76173482647236fce64c73924
artemychych <artemychmain@gmail.com>
2020-09-28 21:56:00 +0900

    table checker added

--- lab_1/php/check.php
+++ lab_1/php/check.php
@@ -3,17 +3,35 @@ session_start();
 $startMicroTime = microtime(true);
 $startWorldTime = date("H:i:s");
 $xValues = $_POST['X'];
-$yValue = $_POST['Y'];
-$rValue = $_POST['R'];
-$result = true;
-$resultColor = $result=="invalid" ? "red" : ($result=="true" ? "green" : "red");
+$yValue = (float) str_replace(",", ".", $_POST['Y']);
+$rValue = (int) $_POST['R'];
+
 $endMicroTime = round((microtime(true) - $startMicroTime)*1e6, 2)." ps";
-echo "<tr>
-                            <td>$xValues</td>
-                            <td>$yValue</td>
-                            <td>$rValue</td>
-                            <td>$startWorldTime</td>
-                            <td>$endMicroTime</td>
-                            <td style='color:$resultColor;'>$result</td>
-                        </tr>";
+
+function check($x, $y, $r){
+    if (($x <= $r && $x >= 0 && $y <= 0 && $y >= -$r) || ($x <= ($r - 2*$y) && $y <= $r/2 && $x <= 0) || (($x*$x + $y*$y) <= $r*$r && $x <= 0 && $y <= 0)){
+        return "<span style='color: #008000'>True</span>";
+    } else {
+        return "<span style='color: #ff0000'>False</span>";
+    }
+}
+
+
+$checkbox = explode(',', $xValues);
+for ($i=0; $i<sizeof($checkbox); $i++) {
+    $checkbox[$i]=(int) trim($checkbox[$i],',');
+}
+
+if (!isset($_SESSION['history'])) {
+    $_SESSION['history'] = array();
+}
+
+for ($i=0; $i<sizeof($checkbox); $i++) {
+    $res = check($checkbox[$i], $yValue, $rValue);
+    $result = array($checkbox[$i], $yValue, $rValue, $res, $startWorldTime, $endMicroTime);
+    array_push($_SESSION['history'], $result);
+    include "table.php";
+
+}
+
 ?>
---
+++ lab_1/php/table.php
@@ -0,0 +1,22 @@
+<table>
+    <thead>
+        <div class="header__item"><a id="x-table" class="filter__link" href="#">X</a></div>
+        <div class="header__item"><a id="y-table" class="filter__link filter__link--number" href="#">Y</a></div>
+        <div class="header__item"><a id="r-table" class="filter__link filter__link--number" href="#">R</a></div>
+        <div class="header__item"><a id="result-table" class="filter__link filter__link--number" href="#">result</a></div>
+        <div class="header__item"><a id="time-table" class="filter__link filter__link--number" href="#">Time</a></div>
+        <div class="header__item"><a id="cr-time-table" class="filter__link filter__link--number" href="#">Benchmark</a></div>
+    <thead>
+    <?php foreach ($_SESSION['history'] as $value) { ?>
+        <div class="table-content">
+            <div class="table-row">
+                <div class="table-data"><?php echo $value[0] ?></div>
+                <div class="table-data"><?php echo $value[1] ?></div>
+                <div class="table-data"><?php echo $value[2] ?></div>
+                <div class="table-data"><?php echo $value[3] ?></div>
+                <div class="table-data"><?php echo $value[4] ?></div>
+                <div class="table-data"><?php echo $value[5] ?></div>
+            </div>
+        </div>
+    <?php }?>
+</table>
